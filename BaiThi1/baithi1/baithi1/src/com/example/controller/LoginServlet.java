b4618ca5bfb98f4378ee281dfc0e0736113b60e0
phamvietkhanh9 <61862710+phamvietkhanh9@users.noreply.github.com>
2020-06-17 20:14:46 +0700

    Update LoginServlet.java

--- BaiThi1/baithi1/baithi1/src/com/example/controller/LoginServlet.java
+++ BaiThi1/baithi1/baithi1/src/com/example/controller/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/logout")
+@WebServlet(name = "LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
